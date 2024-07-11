6f0c24df6bbda23944ed28e87f17a5a3baec299c
marco-scocca <marcoscocca1991@gmail.com>
2024-07-11 08:04:15 +0200

    readme

---
+++ README.md
@@ -0,0 +1,7 @@
+generare 16 numeri casuali da 1 a 100. 
+Al click della casella controllo se il numero è presente nell'array delle bombe
+se il numero si trova lì allora la partita è finita
+altrimenti la casella si colora e posso cliccare altre caselle
+se l'utente clicca su tutte le caselle dove on ci sono le bombe la partita finisce
+altrimenti la partita continua
+
---
+++ js/script.js
@@ -0,0 +1,33 @@
+function createSingleSquare(num sideNumber) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    let sideLength = `calc(100% / ${sideNumber})`;
+    square.style.width = sideLength;
+    square.style.height = sideLength;
+    square.innerText = num + 1;
+    square.addEventListener('click', function () {
+        this.classList.add('clicked');
+        console.log(this.innerText);
+    });
+    return square;
+
+}
+
+function.generateGrid(cellsNumber, sideNumber){
+    const grid = document.getElementById('grid');
+    for (let i = 0; i < cellsNumber; i++) {
+        let item = createSingleSquare(i, sideNumber)
+        grid.append(item);
+    }
+}
+
+function createNewGame() {
+    grid.innerHTML = '';
+    generateGrid(100, 10);
+
+}
+
+const button = document.getElementById('play')
+button.addEventListener('click', function () {
+    createNewGame();
+})
